100a799376a5f0c1835ecadb24387d546cab3158
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2022-09-04 16:31:55 +0300

    Create JadApiMiddleware.php

---
+++ src/JadApiMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Jad\Jad;
+use Jad\Configure;
+use Jad\Map\AnnotationsMapper;
+use Doctrine\ORM\EntityManager;
+
+final class JadApiMiddleware implements MiddlewareInterface
+{
+    private EntityManager $entityManager;
+	
+	private ResponseFactoryInterface $responseFactory;
+	
+	private Configure $configure;
+	
+	private string $path;
+
+    public function __construct(
+		EntityManager $entityManager, 
+		ResponseFactoryInterface $responseFactory,
+		Configure $configure,
+		string $path
+	)
+    {
+        $this->entityManager = $entityManager;
+		$this->responseFactory = $responseFactory;
+		$this->configure = $configure;
+		$this->path = $path;
+    }
+
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+		
+		$rootPath = mb_substr($request
+			->getUri()
+			->getPath(), 0, 9)
+			;
+			
+		if($rootPath === $this->path) {	
+		
+			$jad = new Jad(new AnnotationsMapper($this->entityManager));
+			$jad
+				->setPathPrefix($this->path)
+				;
+	
+			$response = $this
+				->responseFactory
+				->createResponse()
+				;
+	
+			$jad
+				->jsonApiResult()
+				;
+				
+			return $response;
+
+		} else {
+			return $handler->handle($request);
+		}
+    }
+}
